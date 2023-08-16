d0b30eeba1bff615d2b86a20e7ed2aa1977308c8
ruranjo <rubens.rangel.job@gmail.com>
2023-08-16 09:02:47 -0400

    add fix 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://ruranjo.github.io/rick-morty-web-mui-react.github.io'
+  base: '/rick-morty-web-mui-react.github.io/'
 })
